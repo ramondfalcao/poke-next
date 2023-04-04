ebcda4b01b24aa7cbd247e34ca9b8ce6ba908207
Ramond Falcão <ramondfalcao@gmail.com>
2023-04-04 11:01:04 -0300

    Criando Requisição de todos pokemons da api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,34 @@
 import styles from '@/styles/Home.module.css'
 
-export default function Home() {
-  return (<h1>PokeNext</h1>)
+
+export async function getStaticProps() {
+  const maxPokemons = 251
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+  const data = await res.json()
+
+  // add pokemon index
+  data.results.forEach((element, index) => {
+    element.id = index + 1
+  });
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  }
+}
+
+export default function Home({ pokemons }) {
+  return (
+    <div>
+      <h2>Pokenext</h2>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
